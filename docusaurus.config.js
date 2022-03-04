051fb46c5f5445076c4fcfe1c9291b7aff1ea0a3
4lch4 <git@4lch4.email>
2022-03-04 11:40:16 -0600

    feat: start of blog for nova

---
+++ docusaurus.config.js
@@ -0,0 +1,59 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Nova Poetry',
+  tagline: 'The poetry and prose of Nova.',
+  url: 'https://nova-poetry.4lch4.cloud',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: '4lch4', // Usually your GitHub org/user name.
+  projectName: 'Nova-Poetry', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: false,
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: 'https://github.com/4lch4/Nova-Poetry/',
+          routeBasePath: '/'
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css')
+        }
+      })
+    ]
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Nova Poetry',
+        logo: {
+          alt: 'Nova Poetry Logo',
+          src: 'img/logo.svg'
+        }
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} 4lch4 Industries, LLC. Built with Docusaurus.`
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme
+      }
+    })
+}
+
+module.exports = config
